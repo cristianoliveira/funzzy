afb690eb2a699782ba1f423793a2ec882b388ed0
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-03-26 11:48:16 -0300

    fix: args version flag

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate docopt;
 
 pub mod cli;
 use docopt::Docopt;
+use cli::Args;
 
 const USAGE: &'static str = "
 Funzzy the watcher.
@@ -12,11 +13,21 @@ Usage:
   funzzy init
 
 Options:
-  -h --help Shows this message.
+  -h --help     Shows this message.
+  -v --version  Shows version.
 ";
 
 fn main() {
+    let args: Args = Docopt::new(USAGE)
+                            .and_then(|dopt| dopt.decode())
+                            .unwrap_or_else(|e| e.exit());
 
-    println!("Hello, world!");
+    match cli::command(args) {
+       Some(command) => match command.execute() {
+           Ok(()) => println!("Command execute."),
+           Err(err) => println!("{}", err)
+       },
+       None => println!("{}", USAGE)
+    }
 }
 
--- tests/main.rs
+++ tests/main.rs
@@ -1,2 +1,16 @@
 extern crate funzzy;
 mod cli;
+
+
+#[test]
+fn it_returns_some_command() {
+   let mut args = funzzy::cli::Args::new();
+   args.cmd_init = true;
+   assert!(funzzy::cli::command(args).is_some())
+}
+
+#[test]
+fn it_returns_no_command() {
+   let mut args = funzzy::cli::Args::new();
+   assert!(funzzy::cli::command(args).is_none())
+}
