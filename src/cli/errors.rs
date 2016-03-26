c53146a64f68d10b012070858cb43e222539a30b
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-03-26 11:51:25 -0300

    fix: tabs init template

--- src/cli/errors.rs
+++ src/cli/errors.rs
@@ -4,11 +4,17 @@ use std::fmt;
 use std::convert::From;
 use std::error::Error;
 
+
+/// # Errors Handlers
+/// Application custom errors.
+///
+/// All errors possibles should be declared here.
+///
+
 #[derive(Debug)]
 pub enum CliError {
     Io(io::Error),
 }
-
 impl fmt::Display for CliError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
@@ -17,6 +23,7 @@ impl fmt::Display for CliError {
     }
 }
 
+/// Implement here how display the error
 impl error::Error for CliError {
     fn description(&self) -> &str {
         match *self {
@@ -31,6 +38,7 @@ impl error::Error for CliError {
     }
 }
 
+/// How parse the custom error?
 impl From<io::Error> for CliError {
     fn from(error: io::Error) -> CliError {
        CliError::Io(error)
--- src/cli/init.rs
+++ src/cli/init.rs
@@ -9,15 +9,14 @@ pub struct InitCommand;
 impl Command for InitCommand {
     fn execute(&self) -> Result<(), CliError>{
         const DEFAULT_CONTENT: &'static str = "
-            ## Funzzy events file
-            # more details see: http://cristian.github.com/funzzy
-            #
-            # list here all the events and the commands that it should execute
+ ## Funzzy events file
+ # more details see: http://cristian.github.com/funzzy
+ #
+ # list here all the events and the commands that it should execute
 
-            - name: run my tests
-              when: '.'
-              do: 'echo \"It works!\"'
-        ";
+ - name: run my tests
+   when: '.'
+   do: 'echo \"It works!\"' \0";
 
         let mut yaml:File = try!(File::create("events.yaml"));
 
