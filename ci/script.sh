254f1cb59c66840c14e25d9b5000e72e2be0a723
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-03-27 19:18:39 -0300

    chore: ci generate builds

---
+++ ci/script.sh
@@ -0,0 +1,11 @@
+# `script` phase: you usually build, test and generate docs in this phase
+
+set -ex
+
+cargo build --target $TARGET --verbose
+cargo test --target $TARGET
+
+cargo build --target $TARGET --release
+
+# sanity check the file type
+file target/$TARGET/release/antr
