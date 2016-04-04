c88296cf7eaaf3b2250323c0a5d379eec2958a50
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-04-03 23:47:25 -0300

    fix: url version

--- linux-install.sh
+++ linux-install.sh
@@ -1,6 +1,6 @@
 cd /tmp
 
-wget https://github.com/cristianoliveira/funzzy/releases/download/v0.2.4/funzzy-v0.2.5-x86_64-unknown-linux-gnu.tar.gz
+wget https://github.com/cristianoliveira/funzzy/releases/download/v0.2.5/funzzy-v0.2.5-x86_64-unknown-linux-gnu.tar.gz
 
 tar -xf funzzy-v0.2.5-x86_64-unknown-linux-gnu.tar.gz
 
