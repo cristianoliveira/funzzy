e11bc02e54c16fa7501d3657eea5dbbb1fcbdeee
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-10-14 11:58:23 -0300

    version: 0.3.1

--- linux-install.sh
+++ linux-install.sh
@@ -1,9 +1,9 @@
+VERSION="v0.3.1"
+
 cd /tmp
 
-wget https://github.com/cristianoliveira/funzzy/releases/download/v0.3.0/funzzy-v0.3.0-x86_64-unknown-linux-gnu.tar.gz
-
-tar -xf funzzy-v0.3.0-x86_64-unknown-linux-gnu.tar.gz
-
+wget https://github.com/cristianoliveira/funzzy/releases/download/$VERSION/funzzy-v0.3.0-x86_64-unknown-linux-gnu.tar.gz
+tar -xf funzzy-$VERSION-x86_64-unknown-linux-gnu.tar.gz
 sudo cp funzzy /usr/local/bin
 
 echo "Application was installed in /usr/local/bin. To uninstall just do rm /usr/local/bin/funzzy"
