8ed6f2d3b0071065dc4c123b1f9cedb3e5f94092
Cristian Oliveira <contato@cristianoliveira.com.br>
2016-10-14 20:38:43 -0300

    fix: linux install

--- linux-install.sh
+++ linux-install.sh
@@ -2,7 +2,11 @@ VERSION="v0.3.1"
 
 cd /tmp
 
+<<<<<<< Updated upstream
 wget https://github.com/cristianoliveira/funzzy/releases/download/$VERSION/funzzy-v0.3.0-x86_64-unknown-linux-gnu.tar.gz
+=======
+wget https://github.com/cristianoliveira/funzzy/releases/download/$VERSION/funzzy-$VERSION-x86_64-unknown-linux-gnu.tar.gz
+>>>>>>> Stashed changes
 tar -xf funzzy-$VERSION-x86_64-unknown-linux-gnu.tar.gz
 sudo cp funzzy /usr/local/bin
 
