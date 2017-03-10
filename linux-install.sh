e1c2bdf789c6fd2780f0429765f7df3b57b997fb
Cristian Oliveira <contato@cristianoliveira.com.br>
2017-03-09 22:30:02 -0300

    chore: update version 0.3.2

--- linux-install.sh
+++ linux-install.sh
@@ -1,12 +1,8 @@
-VERSION="v0.3.1"
+VERSION="v0.3.2"
 
 cd /tmp
 
-<<<<<<< Updated upstream
-wget https://github.com/cristianoliveira/funzzy/releases/download/$VERSION/funzzy-v0.3.0-x86_64-unknown-linux-gnu.tar.gz
-=======
 wget https://github.com/cristianoliveira/funzzy/releases/download/$VERSION/funzzy-$VERSION-x86_64-unknown-linux-gnu.tar.gz
->>>>>>> Stashed changes
 tar -xf funzzy-$VERSION-x86_64-unknown-linux-gnu.tar.gz
 sudo cp funzzy /usr/local/bin
 
