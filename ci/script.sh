9408e48dcc38bfa2c159fca1827a91e6f5076eda
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-05-21 17:20:51 +0200

    ci: upload release artifacts (#48)

--- ci/script.sh
+++ ci/script.sh
@@ -2,10 +2,22 @@
 
 set -ex
 
+# Add macOS Rust target
+rustup target add $TARGET
+
 cargo build --target $TARGET --verbose
 cargo test --target $TARGET
 
 cargo build --target $TARGET --release
 
+cd target/release
+ls -l
+
+ARTIFACT="funzzy-${RELEASE_TAG:?"Missing release tag"}-${TARGET}.tar.gz"
+
+tar czf "../$ARTIFACT" *
+
+cp "../$ARTIFACT" ../../artifacts/
+
 # sanity check the file type
-file target/$TARGET/release/funzzy
+file funzzy
