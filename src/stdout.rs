6e165f36bd48223e0489320115a1360d07579401
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-06-09 08:44:42 +0200

    chore: show command results with spaces

--- src/stdout.rs
+++ src/stdout.rs
@@ -18,7 +18,7 @@ pub fn verbose(msg: &str, verbose: bool) {
 
 pub fn present_results(results: Vec<Result<(), String>>) {
     let errors: Vec<Result<(), String>> = results.iter().cloned().filter(|r| r.is_err()).collect();
-    println!("------------- Funzzy result --------------");
+    println!("Funzzy results: ---------------------------");
     if !errors.is_empty() {
         println!("Failed tasks: {:?}", errors.len());
         errors.iter().for_each(|err| {
@@ -27,5 +27,6 @@ pub fn present_results(results: Vec<Result<(), String>>) {
     } else {
         println!("All tasks finished successfully.");
     }
-    println!("------------------------------------------");
+    println!("-------------------------------------------");
+    println!("\n");
 }
