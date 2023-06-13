3e795ee5821db01c03f4695ce163e4e21bbc314f
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-06-13 09:28:02 +0200

    test: avoid trigger by output.txt

--- tests/integration/specs/watch-command-test.sh
+++ tests/integration/specs/watch-command-test.sh
@@ -6,26 +6,26 @@ test "it watches the configured rules"
 echo "
 - name: run simple command
   run: echo 'test1'
-  change: \"$WORKDIR/**\"
+  change: \"$WORKDIR/*.txt\"
 
 - name: run different command
   run: echo '__placeholder__' | sed s/placeholder/second_commmand/g
-  change: \"$WORKDIR/**\"
+  change: \"$WORKDIR/*.txt\"
 " > "$WORKDIR"/.onwatch.yaml
 
 touch "$WORKDIR"/test.txt
-touch "$WORKDIR"/output.txt
-$TEST_DIR/funzzy watch --config $WORKDIR/.onwatch.yaml >> $WORKDIR/output.txt &
+touch "$WORKDIR"/output.log
+$TEST_DIR/funzzy watch --config $WORKDIR/.onwatch.yaml >> $WORKDIR/output.log &
 FUNZZY_PID=$!
 
-wait_for_file "$WORKDIR/output.txt"
+wait_for_file "$WORKDIR/output.log"
 
 echo "some content" > "$WORKDIR"/test.txt
 
 touch "$WORKDIR"/test.txt
-assert_file_occurrencies "$WORKDIR/output.txt" "__second_commmand__" 1
+assert_file_occurrencies "$WORKDIR/output.log" "__second_commmand__" 1
 
 touch "$WORKDIR"/test.txt
-assert_file_occurrencies "$WORKDIR/output.txt" "__second_commmand__" 2
+assert_file_occurrencies "$WORKDIR/output.log" "__second_commmand__" 2
 
 cleanup
