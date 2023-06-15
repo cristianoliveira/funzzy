9aed153906b804ff18ef9b9e594de241d4b62b21
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-06-15 10:15:15 +0200

    fix: cancelling task doesn't finish child's spawned processes (#76)

--- src/workers.rs
+++ src/workers.rs
@@ -1,6 +1,10 @@
+extern crate nix;
+
 use crate::cmd::spawn;
 use crate::rules::{self, Rules};
 use crate::stdout;
+use nix::sys::signal::{self, Signal};
+use nix::unistd::Pid;
 use std::sync::mpsc::channel;
 use std::sync::mpsc::{Sender, TryRecvError};
 use std::thread::JoinHandle;
@@ -55,7 +59,10 @@ impl Worker {
                                             verbose,
                                         );
 
-                                        if let Err(err) = child.kill() {
+                                        if let Err(err) = signal::kill(
+                                            Pid::from_raw(child.id() as i32),
+                                            Signal::SIGINT,
+                                        ) {
                                             stdout::error(&format!(
                                                 "failed to kill the task {:?}: {:?}",
                                                 task, err
