ebc217163bb876c11b8284ae632254df8e68d8e4
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-09-30 11:23:30 +0200

    tests: adds test cases for invalid config file

--- src/watches.rs
+++ src/watches.rs
@@ -235,4 +235,38 @@ mod tests {
             ]
         );
     }
+
+    #[test]
+    fn it_returns_an_error_when_fail_to_load_config_file() {
+        assert!(rules::from_yaml(
+            &r#"
+        - name: run tests
+          run: [
+            "yarn test {{filepath}}", 
+            "echo '{{filepath}}' | sed -r 's\/.tsx/\/'" 
+          ]
+          change: 'src/**'
+        "#
+        )
+        .is_err());
+
+        assert!(rules::from_yaml(
+            &r#"
+        - name: run tests
+          run: [
+            "yarn test {{filepath}}", 
+          change: 'src/**'
+        "#
+        )
+        .is_err());
+
+        assert!(rules::from_yaml(
+            &r#"
+        - name: other
+          run: 'cargo test'
+          change: 'test/**'
+        "#
+        )
+        .is_ok());
+    }
 }
