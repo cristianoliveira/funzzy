a011453f2968b0f211ac8c79fda611a28051cbea
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-10-01 08:51:48 +0200

    fix: linux install script whoops

--- linux-install.sh
+++ linux-install.sh
@@ -1,16 +1,15 @@
 CARGO_VERSION="$(curl https://raw.githubusercontent.com/cristianoliveira/funzzy/master/Cargo.toml | grep version | awk -F\" '{print $2}')"
 VERSION="v${1:-$CARGO_VERSION}"
 
-
 echo "Installing funzzy $VERSION"
 
-# cd /tmp
-#
-# wget https://github.com/cristianoliveira/funzzy/releases/download/$VERSION/funzzy-$VERSION-x86_64-unknown-linux-gnu.tar.gz
-# tar -xf funzzy-$VERSION-x86_64-unknown-linux-gnu.tar.gz
-#
-# sudo cp pkg/funzzy /usr/local/bin
-# sudo cp pkg/fzz /usr/local/bin
-#
-# echo "Cli was installed in /usr/local/bin/funzzy and /usr/local/bin/fzz"
-# echo "To uninstall just run 'rm /usr/local/bin/funzzy' and  'rm /usr/local/bin/fzz'"
+cd /tmp
+
+wget https://github.com/cristianoliveira/funzzy/releases/download/$VERSION/funzzy-$VERSION-x86_64-unknown-linux-gnu.tar.gz
+tar -xf funzzy-$VERSION-x86_64-unknown-linux-gnu.tar.gz
+
+sudo cp pkg/funzzy /usr/local/bin
+sudo cp pkg/fzz /usr/local/bin
+
+echo "Cli was installed in /usr/local/bin/funzzy and /usr/local/bin/fzz"
+echo "To uninstall just run 'rm /usr/local/bin/funzzy' and  'rm /usr/local/bin/fzz'"
