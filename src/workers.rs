b334ea674e94e6a217285737af1550a923cb3a75
cristianoliveira <contato@cristianoliveira.com.br>
2024-07-02 17:51:56 +0200

    start implementing the on_event

--- src/workers.rs
+++ src/workers.rs
@@ -9,11 +9,25 @@ use std::sync::mpsc::channel;
 use std::sync::mpsc::{Sender, TryRecvError};
 use std::thread::JoinHandle;
 
+pub enum WorkerEvent {
+    Init,
+    InitThread,
+    TaskCancelled,
+    SpawnTask,
+    TaskCancelling,
+    CancellingDone,
+    TaskCancellingError,
+    Tick,
+    TaskResult,
+}
+
 pub struct Worker {
     canceller: Option<Sender<()>>,
     scheduler: Option<Sender<Vec<String>>>,
 
     consumer: Option<JoinHandle<()>>,
+
+    on_event: Option<fn(WorkerEvent, Result<(), String>)>,
 }
 
 impl Worker {
@@ -147,46 +161,41 @@ impl Worker {
             canceller: Some(tcancel),
             scheduler: Some(tscheduler),
             consumer: Some(consumer),
+            on_event: None,
         }
     }
 
     #[rustfmt::skip]
-    pub fn run(&mut self) -> Result<(), String> {
-        // stdout::verbose("Worker in verbose mode.", verbose);
-        // on_init
+    pub fn run(&mut self, on_event: fn(WorkerEvent, Result<(), String>)) -> Result<(), String> {
         // Unfortunatelly channels can't have multiple receiver so we need to
         // create a channel for each kind of event.
         let (tscheduler, rscheduler) = channel::<Vec<String>>();
         let (tcancel, rcancel) = channel::<()>();
+
+        // stdout::verbose("Worker in verbose mode.", verbose);
+        on_event(WorkerEvent::Init, Ok(()));
 
         let consumer = std::thread::spawn(move || {
             while let Ok(mut tasks) = rscheduler.recv() {
                 let mut results: Vec<Result<(), String>> = vec![];
                 let ignored = rcancel.try_recv();
 
-                // on_event("initial_load")
                 // stdout::verbose(&format!("ignored kill: {:?}", ignored), verbose);
-                todo!("implement on_event");
+                on_event(WorkerEvent::InitThread, Ok(()));
 
                 let mut has_been_cancelled = false;
 
                 while let Some(task) = tasks.pop() {
                     if has_been_cancelled {
-                        // || (fail_fast
-                        //     && !results.clone().into_iter().find(|r| r.is_err()).is_none())
-                        // on_task_fails set has_been_cancelled to true when fail fast
-                        //
-                        // on_event("cancelled")
-                        todo!("implement on_event");
+                        on_event(WorkerEvent::TaskCancelled, Ok(()));
                         break;
                     }
 
                     let mut child = match spawn(&task) {
                         Ok(child) => child,
                         Err(err) => {
-                            // on_event("spawn_task", Err(&format!("failed to create command: {:?}", err)))
                             // stdout::error(&format!("failed to create command: {:?}", err));
-                            todo!("implement on_event");
+                            on_event(WorkerEvent::SpawnTask, Err(format!("failed to spawn the process: {:?}", err)));
                             continue;
                         }
                     };
@@ -201,30 +210,31 @@ impl Worker {
                                 // continue running
                                 match rcancel.try_recv() {
                                     Ok(_) => {
-                                        todo!("implement on_event");
-                                        // on_event("cancelling_init", Ok(&format!("failed to create command: {:?}", err)))
                                         // stdout::verbose(
                                         //     &format!("---- cancelling: {:?} ----", task),
                                         //     verbose,
                                         // );
+                                        on_event(
+                                            WorkerEvent::TaskCancelling,
+                                            Ok(()),
+                                        );
 
                                         if let Err(err) = signal::kill(
                                             Pid::from_raw(child.id() as i32),
                                             Signal::SIGINT,
                                         ) {
-                                            todo!("implement on_event");
-                                            // on_event(
-                                            //   "cancelling_init", 
-                                            //   Err(&format!(
-                                            //     "failed to kill the task {:?}: {:?}",
-                                            //     task,
-                                            //     err
-                                            //   ));
-                                            // );
                                             // stdout::error(&format!(
                                             //     "failed to kill the task {:?}: {:?}",
                                             //     task, err
                                             // ));
+                                            on_event(
+                                              WorkerEvent::TaskCancellingError,
+                                              Err(format!(
+                                                "failed to kill the current running process {:?}: {:?}",
+                                                task,
+                                                err
+                                              ))
+                                            );
                                         }
 
                                         if let Ok(status) = child.wait() {
