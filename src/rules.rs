802f8a36c0bdb064cd85e860cff1a312c349cde2
cristianoliveira <contato@cristianoliveira.com.br>
2025-04-24 10:37:09 +0200

    fix ignore /.git/** path

--- src/rules.rs
+++ src/rules.rs
@@ -60,7 +60,7 @@ impl Rules {
             // FIXME this is clearly slow and can be optimized
             if watch.starts_with("{{git") {
                 // NOTE: not sure if this must be inplicitly ignored
-                if path.starts_with(".git/") {
+                if path.contains("/.git/") {
                     return false;
                 }
                 
